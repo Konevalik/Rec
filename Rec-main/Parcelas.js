4c6d9e78e2871dba3f737610c625218a30ad6959
Konevalik <136601254+Konevalik@users.noreply.github.com>
2023-12-06 10:03:38 -0300

    Add files via upload

---
+++ Rec-main/Parcelas.js
@@ -0,0 +1,11 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector ("pre")
+
+//ouvinte
+frm.addEventListener("submit", (e)=>{
+    e.preventDefault
+
+    const valor = Number(frm.inValor.value)
+    const num = Number(frm.inParcela.value)
+    
+})
