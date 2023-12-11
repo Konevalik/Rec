f729e1c1753030eaa66e276ff1b1fe447ec575c3
Konevalik <136601254+Konevalik@users.noreply.github.com>
2023-12-11 08:24:14 -0300

    Add files via upload

---
+++ divisores.js
@@ -0,0 +1,18 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector ("h3")
+
+//ouvinte
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const num = Number (frm.inNumero.value)
+    let resposta= " " //não precia verificar se é ivisivel por 1
+
+    for(let i = 1; i <= num/2; i++){  //não precisa copar mais que a metade do numero.
+if (num % i == 0){
+    resposta = resposta + i + ","
+    
+     }
+   }
+  resp.innerText = `Divisores de ${num}: ${resposta}${num}.`
+})
